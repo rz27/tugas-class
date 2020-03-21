f9b9772181ad374e41bc7d56616c3ade9a3a3576
rz27 <Feizalreza29@gmail.com>
2020-03-21 19:51:51 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taxi
+{
+    class taxi
+    {
+        public string DriverName { get; set; }
+
+        public bool OnDuty { get; set; }
+
+        public int NumPassenger { get; set; }
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+
+
+        public void PickUpPassenger()
+
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
---
+++ classt.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek taxi
+            taxi taxi = new taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Anton";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
